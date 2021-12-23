bd9061eeb76325400819ba69fde59391c6dbc2e6
Gurtej736l <gurtej154@gmail.com>
2021-12-23 11:10:17 +1030

    inital commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  // res.sendFile(__dirname + "/public/html/index.html");
+  res.sendFile(path.join(__dirname, "/public/html/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Now listening on port: http://localhost:${PORT}`);
+});
